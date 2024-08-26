82b63879e2bfffeb2c3a98a707d30ba273f205c5
karengonzalezl <karenvgonzalez19@gmail.com>
2024-08-26 17:47:43 -0400

    Challenge

---
+++ app.js
@@ -0,0 +1,39 @@
+var llavesDeEncriptacion={
+    a: "ai",
+    e: "enter",
+    i: "imes",
+    o: "ober",
+    u: "ufat",
+};
+
+var encriptacionTextoDeUsuario;
+function encriptar (Texto);
+function desencriptar (encriptarTexto);
+    let (desencriptartexto == encriptartexto);
+
+if (llavesDeEncriptacion == encriptacionTextoDeUsuario);
+
+function limpiarCaja() {
+    let valorCaja= document.querySelector('inputId');
+    valorCaja.value="";
+}
+
+
+function selectId (id){
+    return document.getElementById (id);    
+}
+
+var activeBottom(encriptar) != activeBottom(desencriptar);
+
+const = {
+    encriptarButton: selectId (button-encriptar),
+    desencriptarButton: selectId (button-desencriptar),
+    inputTexto: selectId (input_texto),
+    outputTexto: selectId (output_texto),
+};
+
+let errorMensaje ="Escriba en letras minusculas, sin acentos y evite usar caracteres especiales";
+const input_text= document.querySelector("input_text");
+const outputTexto= document.querySelector("output_texto");
+
+
